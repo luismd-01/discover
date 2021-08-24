1198db945d49fb0267182dff3e00da51683161cb
luismd-01 <88732447+luismd-01@users.noreply.github.com>
2021-08-24 14:19:51 -0400

    First commit

---
+++ js/code.js
@@ -0,0 +1,54 @@
+gsap.registerPlugin(ScrollTrigger);
+		const locoScroll = new LocomotiveScroll({
+			el: document.querySelector("[data-scroll-container]"),
+			smooth: true,
+			getSpeed: true,
+			getDirection: true,
+            inertia: 0.75,
+			useKeyboard: true
+		});
+
+		locoScroll.update();
+
+//console.log(ScrollTrigger.scrollerProxy)
+
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("[data-scroll-container]", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("[data-scroll-container]").style.transform ? "transform" : "fixed"
+});
+
+
+// Layered Pinning from Bottom
+gsap.to(".panel:not(:last-child)", {
+  yPercent: -100, 
+  ease: "none",
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: "#container",
+    start: "top top",
+    end: "+=300%",
+    scrub: true,
+    pin: true,
+    scroller: '[data-scroll-container]'  // * //     
+  }
+});
+
+
+gsap.set(".panel", {zIndex: (i, target, targets) => targets.length - i});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
